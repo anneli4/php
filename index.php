4778567d9a254a1f0ac2977dbe5810bdde7fff60
Anneli Peep <anneli.peep@gmail.com>
2025-06-20 09:11:36 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $name ='Anneli';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello Anneli</h1>
+</body>
+</html>
